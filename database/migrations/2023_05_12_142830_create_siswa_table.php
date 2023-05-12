9c1aed8c4bf3690d420209f78601277d405bc167
felicianlvn <nelvinafelicia@gmail.com>
2023-05-12 23:25:08 +0700

    Siswa Migration & Seeder

---
+++ database/migrations/2023_05_12_142830_create_siswa_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswa', function (Blueprint $table) {
+            $table->char('nis', 4)->primary();
+            $table->tinyText('nama');
+            $table->enum("jenis_kelamin", ["Perempuan", "Laki-Laki"]);
+            $table->tinyText("kelas");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siswa');
+    }
+};
